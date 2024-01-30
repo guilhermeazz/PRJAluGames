6897102ae52750c2c9bc1c4cea7fea6e8d6ac112
Guilherme Albuquerque <guialbuzapa@gmail.com>
2024-01-30 10:38:25 -0300

    implementação extras

--- js/app.js
+++ js/app.js
@@ -1,15 +1,34 @@
+let quantidade = 1
+
 function alterarStatus(id) {
     let game = document.getElementById(`game-${id}`);
     let imagem = game.querySelector('.dashboard__item__img');
     let botao = game.querySelector('.dashboard__item__button');
+    let nome = game.querySelector('.dashboard__item__name');
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+        // Adiciona uma confirmação antes de devolver o jogo
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nome.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
     }
+    jogosAlugados(id);
+}
+
+function jogosAlugados(id) {
+    let game = document.getElementById(`game-${id}`);
+    let botao = game.querySelector('.dashboard__item__button');
+
+    if (botao.classList.contains('dashboard__item__button--return')) {
+        quantidade = quantidade + 1;
+    }else{
+        quantidade = quantidade - 1;
+    }
+    console.log(`${quantidade} jogos alugados`)
 }
